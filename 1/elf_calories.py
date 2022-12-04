1ead07aa587199aef17d2dd6bfc9ab66ee4f1db4
Jostein Sætre Løfsgaard <jslofsgaard@gmail.com>
2022-12-04 13:36:26 +0100

    Refactor day 1 solution

--- 1/elf_calories.py
+++ 1/elf_calories.py
@@ -1,6 +1,15 @@
 from typing import Union, List
 
 
+class CalorieList:
+    def __init__(self, calories: List[int]):
+        self.calories = calories
+
+    @property
+    def total(self):
+        return sum(self.calories)
+
+
 def parse_line(line: str) -> Union[int, None]:
     """Read a line and return an integer if the line can be converted
     to it, None otherwise.
@@ -11,73 +20,41 @@ def parse_line(line: str) -> Union[int, None]:
         return None
 
 
-class Elf:
-    def __init__(self, aoc_input):
-        self.set_calories(aoc_input)
+def get_calorie_lists(puzzle_input: str = 'input.txt') -> List[CalorieList]:
+    """Construct a list of CalorieLists from the input located at puzzle_input.
+    """
+    with open(puzzle_input) as f:
+        items = []
+        line = f.readline()
+        while line:
+            calories = []
+            calorie = parse_line(line)
+            while calorie:
+                calories.append(calorie)
+                calorie = parse_line(f.readline())
 
-    def set_calories(self, aoc_input):
-        self.calories = []
+            items.append(CalorieList(calories))
+            line = f.readline()
 
-        calorie = parse_line(aoc_input.readline())
-        while calorie:
-            self.calories.append(calorie)
-            calorie = parse_line(aoc_input.readline())
-
-    def get_total_calories(self):
-        return sum(self.calories)
-
-
-class ElfBand:
-    def __init__(self):
-        self.build_from_input()
-
-    def build_from_input(self, aoc_input: str = 'input') -> List[Elf]:
-        """Read the input and construct list of Elfs."""
-        with open(aoc_input) as f:
-            self.band = []
-            elf = Elf(f)
-            while elf.calories:
-                self.band.append(elf)
-                elf = Elf(f)
-
-    def get_max_elf(self) -> Union[Elf, None]:
-        """Returns the Elf with the highest calorie count, None if no
-        elfs are to be found.
-        """
-        if self.band:
-            max = 0
-            for elf in self.band:
-                if elf.get_total_calories() > max:
-                    max = elf.get_total_calories()
-                    max_elf = elf
-            return max_elf
-        else:
-            return None
-
-    def sort_elfs(self):
-        """Sort the group of Elfs based on calorie carried."""
-        self.band.sort(
-            key=lambda elf: elf.get_total_calories(),
-            reverse=True
-        )
+        return items
 
 
 if __name__ == '__main__':
-    elfs = ElfBand()
-    elfs.sort_elfs()
-    max_calories = elfs.band[0].get_total_calories()
-    top_three_total = sum(
-        map(
-            lambda elf: elf.get_total_calories(),
-            elfs.band[0:3]
-        )
+    calorie_lists = sorted(
+            get_calorie_lists(),
+            key=lambda calorie_list: calorie_list.total,
+            reverse=True
     )
+    top_three_total = sum(map(
+            lambda calorie_list: calorie_list.total,
+            calorie_lists[0:3]
+    ))
 
-    print(f'Max: {max_calories}')
+    print(f'Max: {calorie_lists[0].total}')
     print(
         'Top three: '
-        f'{elfs.band[0].get_total_calories()}, '
-        f'{elfs.band[1].get_total_calories()}, '
-        f'{elfs.band[2].get_total_calories()}'
+        f'{calorie_lists[0].total}, '
+        f'{calorie_lists[1].total}, '
+        f'{calorie_lists[2].total}'
     )
     print(f'Top three total: {top_three_total}')
