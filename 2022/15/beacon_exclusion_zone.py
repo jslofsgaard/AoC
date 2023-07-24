2de59e8a721b118108fdc4c2dca1df9a7b40b720
Jostein Sætre Løfsgaard <jslofsgaard@gmail.com>
2023-07-24 15:17:27 +0200

    2022 - 15: complete part 2
    Not happy about how the code looks, but it happy about how it
    performs. Maybe I'll revist this one day to speedup and cleanup.

--- 2022/15/beacon_exclusion_zone.py
+++ 2022/15/beacon_exclusion_zone.py
@@ -1,8 +1,9 @@
+import multiprocessing
 from typing import Iterable
 
-
 Coordinate = tuple[int, int]
-HORIZ = 2000000
+HORIZ = 2_000_000
+BOUND = 4_000_000
 
 
 def parse_input(path="input.txt") -> list["Sensor"]:
@@ -43,6 +44,18 @@ class Sensor:
     def is_covered(self, x: int, y: int) -> bool:
         return self.distance(x, y) <= self.beacon_distance
 
+    def cross_horizontal(self, x: int, y: int) -> Coordinate:
+        res = (
+            self.location[0] + (self.beacon_distance - abs(y - self.location[1])) + 1,
+            y,
+        )
+        assert self.distance(*res) == self.beacon_distance + 1
+        return res
+
+
+class GridError(Exception):
+    pass
+
 
 class Grid:
     def __init__(self, sensors: Iterable[Sensor]):
@@ -71,6 +84,49 @@ class Grid:
     def is_beacon(self, x: int, y: int) -> bool:
         return any(sensor.beacon == (x, y) for sensor in self.sensors)
 
+    # Must be at the top level for multiprocessing
+    def _line_search(self, args: tuple[int, int, int]) -> Coordinate | None:
+        x_min, x_max, y = args
+        assert x_min < x_max
+
+        point = (x_min, y)
+        while self.is_covered(*point):
+            point = max(
+                (
+                    sensor.cross_horizontal(*point)
+                    for sensor in self.sensors
+                    if sensor.is_covered(*point)
+                ),
+                key=lambda candidate: candidate[0] - point[0],
+            )
+            if x_max < point[0]:
+                return None
+
+        return point
+
+    def distress_beacon_search(
+        self, x_range: Coordinate, y_range: Coordinate
+    ) -> Coordinate:
+        x_min, x_max = x_range[0], x_range[1]
+        y_min, y_max = y_range[0], y_range[1]
+        assert x_min < x_max
+        assert y_min < y_max
+
+        with multiprocessing.Pool(max(multiprocessing.cpu_count() - 2, 1)) as pool:
+            for solution in pool.imap_unordered(
+                self._line_search,
+                ((x_min, x_max, y) for y in range(y_min, y_max + 1)),
+                chunksize=BOUND // 40,
+            ):
+                if solution is not None:
+                    return solution
+            else:
+                raise GridError("No distress beacon found")
+
+
+def tuning_frequency(x: int, y: int) -> int:
+    return x * BOUND + y
+
 
 if __name__ == "__main__":
     # Part 1
@@ -82,4 +138,10 @@ if __name__ == "__main__":
     )
     print(
         f"In the row y={HORIZ} the total number of positions which cannot contain a beacon is: {covered_positions}"
+    )
+
+    # Part 2
+    distress_beacon = grid.distress_beacon_search((0, BOUND), (0, BOUND))
+    print(
+        f"The distress beacon is at {distress_beacon}, its tuning frequency is: {tuning_frequency(*distress_beacon)}"
     )
